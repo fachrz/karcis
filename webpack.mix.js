8295c3d0ced23b9060abe05c10029a7f6226efdb
fachrz <rfachru3@outlook.com>
2022-02-09 17:36:12 +0700

    remove unused scss file

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,5 +19,4 @@ mix.js('resources/js/pesawat_app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/login.scss', 'public/css')
    .sass('resources/sass/register.scss', 'public/css')
-   .sass('resources/sass/sb-admin.scss', 'public/css')
-   .sass('resources/sass/mail.scss', 'public/css');
+   .sass('resources/sass/sb-admin.scss', 'public/css');
